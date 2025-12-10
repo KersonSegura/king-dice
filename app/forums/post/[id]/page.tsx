6051f76b08ebb24514d3e88cd56296116f18a79c
Kerson Segura <kersonseguraarias@gmail.com>
2025-12-10 13:16:28 -0600

    Fix JSX structure error in post detail page: correct div nesting

--- app/forums/post/[id]/page.tsx
+++ app/forums/post/[id]/page.tsx
@@ -815,7 +815,6 @@ export default function PostDetailPage() {
                         )}
                       </div>
                     )}
-                  </div>
                     <div className="flex justify-end mt-2">
                       <button
                         onClick={handleCreateComment}
