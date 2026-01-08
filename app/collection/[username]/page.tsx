3e7c60859f08453e75a33d62312a7be7d8086d4a
Kerson Segura <kersonseguraarias@gmail.com>
2026-01-07 19:09:36 -0600

    Remove unused useMemo import

--- app/collection/[username]/page.tsx
+++ app/collection/[username]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect, useCallback, useMemo } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
